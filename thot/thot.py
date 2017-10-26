3291128fccc02f46c59b3ba16da9b6e34ffbf74c
Mr Boo Grande <jordan99oliver@outlook.com>
2017-10-26 21:19:52 +0100

    Cleaner code

--- thot/thot.py
+++ thot/thot.py
@@ -4,9 +4,7 @@ from .utils.dataIO import fileIO
 from random import choice as randchoice
 import os
 
-
 class Thot:
-
     """Banana's Thot Cog (Based off of Airenkun's Insult Cog)"""
     def __init__(self, bot):
         self.bot = bot
@@ -15,9 +13,7 @@ class Thot:
     @commands.command(pass_context=True, no_pm=True)
     async def thot(self, ctx, user : discord.Member=None):
         """Determines if a user is a thot or not"""
-
-        msg = ' '
-        await self.bot.say(user.mention + msg + randchoice(self.thotchoices))
+        await self.bot.say("{} {}".format(user.mention, randchoice(self.thotchoices)))
 
 def setup(bot):
     bot.add_cog(Thot(bot))
